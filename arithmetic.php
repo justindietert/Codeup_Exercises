a875ac86a6a5004aead1e684925fdb3c9565effe
Justin Dietert <justin.dietert@gmail.com>
2015-03-12 09:23:18 -0500

    passed real numbers to functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,17 +1,25 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
+
+add(1, 2);
+
+subtract(3, 4);
+
+multiply(5, 6);
+
+divide(7, 8);
