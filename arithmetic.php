73b2fbaa5859a42d6254124beb1cdab0b66d2acf
Justin Dietert <justin.dietert@gmail.com>
2015-03-12 09:29:36 -0500

    added modulus function and variables outside functions to check their effect on the functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,8 @@
 <?php
 
+$a = 100; // These variables are not accessible inside the functions below.
+$b = 200; //
+
 function add($a, $b) {
     echo $a + $b . PHP_EOL;
 }
@@ -16,6 +19,10 @@ function divide($a, $b) {
     echo $a / $b . PHP_EOL;
 }
 
+function modulus($a, $b) {
+    echo $a % $b . PHP_EOL;
+}
+
 add(1, 2);
 
 subtract(3, 4);
@@ -23,3 +30,5 @@ subtract(3, 4);
 multiply(5, 6);
 
 divide(7, 8);
+
+modulus(9, 10);
