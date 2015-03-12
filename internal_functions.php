6d0a2c644e7805c995d5572dc0894283774dd5cb
Justin Dietert <justin.dietert@gmail.com>
2015-03-12 15:05:18 -0500

    completed internal functions exercise

--- internal_functions.php
+++ internal_functions.php
@@ -1,10 +1,32 @@
 <?php
 
-$nothing = null;
-$something = '';
+$null = null;
+$blankString = '';
 $array = array(1, 2, 3);
+$emptyArray = [];
+$arrayWithKeyAndValueSetToBlank = ['key' => ''];
 
 // Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+function setOrEmpty($x, $var_name) {
+
+    if (empty($x) && isset($x)) {
+        return "$var_name is EMPTY and SET";
+    }
+
+    if (empty($x)) {
+        return "$var_name is EMPTY";
+    } 
+
+    if (isset($x)) {
+        return "$var_name is SET";
+    } 
+
+}
+
+echo PHP_EOL . setOrEmpty($null, '$null') . PHP_EOL . PHP_EOL;
+echo setOrEmpty($blankString, '$blankString') . PHP_EOL . PHP_EOL;
+echo setOrEmpty($emptyArray, '$emptyArray') . PHP_EOL . PHP_EOL;
+echo setOrEmpty($arrayWithKeyAndValueSetToBlank, '$arrayWithKeyAndValueSetToBlank') . PHP_EOL . PHP_EOL;
 
 // TEST: If var $nothing is set, display '$nothing is SET'
 
@@ -13,5 +35,7 @@ $array = array(1, 2, 3);
 // TEST: If var $something is set, display '$something is SET'
 
 // Serialize the array $array, and output the results
+echo serialize($array) . PHP_EOL . PHP_EOL;
 
 // Unserialize the array $array, and output the results
+print_r(unserialize(serialize($array))) . PHP_EOL;
