44f292a2f330967b951f13f27fc4cbbe0f58042e
Justin Dietert <justin.dietert@gmail.com>
2015-03-12 13:02:20 -0500

    expanded validation checks and separated error messages into separate functions

--- arithmetic.php
+++ arithmetic.php
@@ -4,44 +4,71 @@ $a = 100; // These variables are not accessible inside the functions below.
 $b = 200; //
 
 function add($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (is_numeric($a) && is_numeric($b) && $b !== 0 && !is_string($a) && !is_string($b)) {
         echo $a + $b . PHP_EOL;
-    } else {
-        echo "Both arguments must be numbers.\n";
+    } 
+
+    if (!is_numeric($a) || !is_numeric($b) || is_string($a) || is_string($b)) {
+        errorMessageNum($a, $b);
     }
 }
 
 function subtract($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (is_numeric($a) && is_numeric($b) && $b !== 0 && !is_string($a) && !is_string($b)) {
         echo $a - $b . PHP_EOL;
-    } else {
-        echo "Both arguments must be numbers.\n";
+    } 
+
+    if (!is_numeric($a) || !is_numeric($b) || is_string($a) || is_string($b)) {
+        errorMessageNum($a, $b);
     }
 }
 
 function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (is_numeric($a) && is_numeric($b) && $b !== 0 && !is_string($a) && !is_string($b)) {
         echo $a * $b . PHP_EOL;
-    } else {
-        echo "Both arguments must be numbers.\n";
+    } 
+
+    if (!is_numeric($a) || !is_numeric($b) || is_string($a) || is_string($b)) {
+        errorMessageNum($a, $b);
     }
 }
 
 function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (is_numeric($a) && is_numeric($b) && $b !== 0 && !is_string($a) && !is_string($b)) {
         echo $a / $b . PHP_EOL;
-    } else {
-        echo "Both arguments must be numbers.\n";
+    } 
+
+    if (!is_numeric($a) || !is_numeric($b) || is_string($a) || is_string($b)) {
+        errorMessageNum($a, $b);
+    }
+
+    if($b === 0){
+        errorMessageZero($b);
     }
 }
 
 function modulus($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (is_numeric($a) && is_numeric($b) && $b !== 0 && !is_string($a) && !is_string($b)) {
         echo $a % $b . PHP_EOL;
-    } else {
-        echo "Both arguments must be numbers.\n";
+    } 
+
+    if (!is_numeric($a) || !is_numeric($b) || is_string($a) || is_string($b)) {
+        errorMessageNum($a, $b);
+    }
+
+    if($b === 0){
+        errorMessageZero($b);
     }
 }
+
+function errorMessageNum($x, $y) {
+    echo "Both arguments, '$x' and '$y', must be numbers. Do not pass a number as a string.\n";
+}
+
+function errorMessageZero($y) {
+    echo "Cannot divide by $y.\n";
+}
+
 
 add(1, 2);
 
@@ -49,6 +76,6 @@ subtract(3, 4);
 
 multiply(5, 6);
 
-divide(7, 8);
+divide('7', 0);
 
-modulus(9, 10);
+modulus(20, 0);
